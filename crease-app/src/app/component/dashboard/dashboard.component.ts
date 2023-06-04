289bf4044b1eef2ca76ac76c5915653cedddb8af
TruongThanhTungDev <truongthanhtung2701.dev@gmail.com>
2023-06-05 01:52:26 +0700

    test login

--- crease-app/src/app/component/dashboard/dashboard.component.ts
+++ crease-app/src/app/component/dashboard/dashboard.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'dashboard-page',
   templateUrl: 'dashboard.component.html',
   styleUrls: [],
 })
-export class Dashboard {}
+export class Dashboard {
+  constructor(private router: Router) {}
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['/login']);
+  }
+}
