8732dca8b5507a0d0053a21ff9d7ae7176ccf519
TruongThanhTungDev <truongthanhtung2701.dev@gmail.com>
2023-06-18 23:46:28 +0700

    ban ghi chi phi

--- crease-app/src/app/pages/ban-ghi-chi-phi/ban-ghi-chi-phi.component.ts
+++ crease-app/src/app/pages/ban-ghi-chi-phi/ban-ghi-chi-phi.component.ts
@@ -1,9 +1,11 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { LoadingController, ModalController } from '@ionic/angular';
 import { Store } from '@ngrx/store';
 import * as moment from 'moment';
 import { LocalStorageService } from 'ngx-webstorage';
 import { DanhMucService } from 'src/app/danhmuc.services';
+import { Plugin } from 'src/app/shared/utils/plugins';
 
 @Component({
   selector: 'costRecord-component',
@@ -11,7 +13,10 @@ import { DanhMucService } from 'src/app/danhmuc.services';
 })
 export class CostRecordComponent implements OnInit {
   REQUEST_URL = '/api/v1/cost';
+  costType: any;
+  shopCode = '';
   listData: any;
+  info: any;
   selectedItem: any;
   isToastOpen: any;
   isShowSelectDelete = false;
@@ -19,6 +24,7 @@ export class CostRecordComponent implements OnInit {
   isOpenDeleteModal = false;
   isOpenFilterModal = false;
   isBackHeader: any;
+  plugins = new Plugin();
   dateRange = {
     startDate: moment().startOf('month'),
     endDate: moment().endOf('month'),
@@ -44,16 +50,75 @@ export class CostRecordComponent implements OnInit {
     private store: Store<any>,
     private modal: ModalController
   ) {
+    this.info = this.localStorage.retrieve('authenticationtoken');
     this.store.subscribe((state) => {
       this.isBackHeader = state.common.isBackHeader;
       this.selectedItem = null;
     });
   }
   ngOnInit() {
+    if (this.info.role === 'admin') {
+      this.shopCode = this.localStorage.retrieve('shop').code;
+    }
     this.loadData();
   }
-  async loadData() {}
-
+  async loadData() {
+    var date = JSON.parse(JSON.stringify(this.dateRange));
+    date.endDate = date.endDate.replace('23:59:59', '00:00:00');
+    const payload = {
+      page: 0,
+      size: 10000,
+      filter: this.searchData(),
+      sort: ['id', 'asc'],
+    };
+    await this.isLoading();
+    this.dmService.query(payload, `${this.REQUEST_URL}`).subscribe(
+      (res: HttpResponse<any>) => {
+        if (res.status === 200) {
+          this.loading.dismiss();
+          this.listData = res.body.RESULT.content
+            .map((item: any) => {
+              return {
+                ...item,
+                costName: item.costType ? item.costType.name : '',
+                costId: item.costType ? item.costType.id : '',
+                fromDate: moment(item.fromDate, 'YYYYMMDD').format(
+                  'DD/MM/YYYY'
+                ),
+                toDate: moment(item.toDate, 'YYYYMMDD').format('DD/MM/YYYY'),
+              };
+            })
+            .sort((a: any, b: any) => b.id - a.id);
+        } else {
+          this.loading.dismiss();
+          this.isToastOpen = true;
+          this.messageToast = res.body.MESSAGE
+            ? res.body.MESSAGE
+            : 'Có lỗi xảy ra vui lòng thử lại';
+        }
+      },
+      () => {
+        this.isToastOpen = true;
+        this.messageToast = 'Có lỗi xảy ra vui lòng thử lại';
+        this.loading.dismiss();
+      }
+    );
+  }
+  searchData() {
+    // this.spinner.show();
+    var date = JSON.parse(JSON.stringify(this.dateRange));
+    let startDate = moment(date.startDate, 'YYYYMMDD').format('YYYYMMDD');
+    let endDate = moment(date.endDate, 'YYYYMMDD').format('YYYYMMDD');
+    const filter = [];
+    filter.push('id>0');
+    filter.push(`shopCode==${this.shopCode}`);
+    if (this.costType) {
+      filter.push(`costType.id==${this.costType}`);
+    }
+    if (startDate) filter.push(`fromDate>=${startDate}`);
+    if (endDate) filter.push(`toDate<=${endDate}`);
+    return filter.join(';');
+  }
   filterDate(e: any) {}
 
   addCostRecord() {}
@@ -82,4 +147,14 @@ export class CostRecordComponent implements OnInit {
     }
   }
   openModalFilter(open: boolean) {}
+
+  public async isLoading() {
+    const isLoading = await this.loading.create({
+      spinner: 'circles',
+      keyboardClose: true,
+      message: 'Đang tải',
+      translucent: true,
+    });
+    return await isLoading.present();
+  }
 }
