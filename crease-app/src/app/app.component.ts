f4af3e955c791e42578dbf5898249183ac9904ab
TruongThanhTungDev <truongthanhtung2701.dev@gmail.com>
2023-06-21 19:38:06 +0700

    update UI

--- crease-app/src/app/app.component.ts
+++ crease-app/src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { StatusBar, StatusBarStyle } from '@capacitor/status-bar';
+import { isPlatform } from '@ionic/angular';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    StatusBar.setBackgroundColor({
+      color: '#006eb9e6',
+    });
+  }
 }
