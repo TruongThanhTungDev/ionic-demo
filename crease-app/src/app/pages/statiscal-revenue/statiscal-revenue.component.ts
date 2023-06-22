f824f49fe3fd2cb1ccec2a350249f1b959ebf60a
TruongThanhTungDev <truongthanhtung2701.dev@gmail.com>
2023-06-22 23:59:31 +0700

    update UI

--- crease-app/src/app/pages/statiscal-revenue/statiscal-revenue.component.ts
+++ crease-app/src/app/pages/statiscal-revenue/statiscal-revenue.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment';
+import { LocalStorageService } from 'ngx-webstorage';
+import { DanhMucService } from 'src/app/danhmuc.services';
 
 @Component({
   selector: 'statiscal-revenue-component',
   templateUrl: './statiscal-revenue.component.html',
 })
-export class StatiscalRevenue {
+export class StatiscalRevenue implements OnInit {
+  SHOP_URL = '/api/v1/shop';
+  REQUEST_URL = '/api/v1/data';
   typeShow = 1;
   optionChart = 1;
   startDate: any;
@@ -13,9 +18,44 @@ export class StatiscalRevenue {
   today = new Date();
   month = this.today.getMonth() + 1;
   year = this.today.getFullYear();
-
+  info: any;
+  shopCode = '';
+  shopList: any;
+  constructor(
+    private dmService: DanhMucService,
+    private localStorage: LocalStorageService
+  ) {
+    this.info = this.localStorage.retrieve('authenticationtoken');
+  }
+  ngOnInit() {
+    if (this.info.role === 'admin') {
+      this.shopCode = this.localStorage.retrieve('shop')
+        ? this.localStorage.retrieve('shop').code
+        : '';
+      this.statistic();
+    } else {
+      this.loadShopList();
+    }
+  }
   async loadData() {}
-
+  public loadShopList() {
+    this.dmService
+      .getOption(
+        null,
+        this.SHOP_URL,
+        '/search?filter=id>0;status==1&sort=id,asc&size=1000&page=0'
+      )
+      .subscribe(
+        (res: HttpResponse<any>) => {
+          this.shopList = res.body.RESULT.content;
+          this.shopCode = this.shopList[0].code;
+          this.statistic();
+        },
+        () => {
+          console.error();
+        }
+      );
+  }
   statistic() {
     if (this.typeShow == 1) {
       let date = moment().format('YYYY') + '/' + this.month;
